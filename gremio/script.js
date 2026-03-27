a3034a9412a4ff5e583100952efe3591ff672221
vicenzo <vicenzocioato8@gmail.com>
2026-03-26 23:28:16 -0300

    Conclusão total do projeto base.

---
+++ gremio/script.js
@@ -0,0 +1,29 @@
+const textocima = document.getElementById("textocima")
+const textomeio = document.getElementById("textomeio")
+const textobaixo = document.getElementById("textobaixo")
+
+const faixazul = document.getElementById("faixazul")
+const faixabranca = document.getElementById("faixabranca")
+const faixapreta = document.getElementById("faixapreta")
+
+textocima.addEventListener("input", () => {
+    faixazul.innerText = textocima.value.toUpperCase()
+});
+textomeio.addEventListener("input", () => {
+    faixabranca.innerText = textomeio.value.toUpperCase()
+});
+textobaixo.addEventListener("input", () => {
+    faixapreta.innerText = textobaixo.value.toUpperCase()
+});
+
+const botao = document.getElementById('download')
+const div = document.getElementById('bandeira')
+
+botao.addEventListener("click", () => {
+  html2canvas(div).then(canvas => {
+    const link = document.createElement("a");
+    link.download = "imagem.png";
+    link.href = canvas.toDataURL("image/png");
+    link.click();
+  });
+});
